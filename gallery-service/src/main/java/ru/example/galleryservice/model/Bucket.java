80ba99b0cb1d391170072415fd2fefb857f75f5d
shaplovd@gmail.com <shaplovd@gmail.com>
2021-02-07 22:15:15 +0300

    @feature add Gallery Server

---
+++ gallery-service/src/main/java/ru/example/galleryservice/model/Bucket.java
@@ -0,0 +1,31 @@
+package ru.example.galleryservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+/**
+ * Модель с темами для изучения.
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@Document(collation = "buckets")
+public class Bucket {
+
+    @Id
+    private String id;
+
+    @NotBlank
+    @Size(max = 10)
+    private String title;
+
+    private String description;
+    private int personalNumber;
+    private String imageLink;
+}
