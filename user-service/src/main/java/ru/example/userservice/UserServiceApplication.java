90add72ff6f2a63efc94c71e0a5bdcc8027f8261
shaplovd@gmail.com <shaplovd@gmail.com>
2021-05-02 01:09:48 +0300

    Добавлена балансировка нагрузки Ribbon

--- user-service/src/main/java/ru/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/ru/example/userservice/UserServiceApplication.java
@@ -4,13 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import ru.example.userservice.config.RibbonConfiguration;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableEurekaClient
+@RibbonClient(name = "movie-service", configuration = RibbonConfiguration.class)
 public class UserServiceApplication {
 
     public static void main(String[] args) {
@@ -22,5 +27,13 @@ public class UserServiceApplication {
     @LoadBalanced // Load balance between service instances running at different ports.
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector())
+                .baseUrl("http://localhost:8081")
+                .build();
     }
 }
---
+++ user-service/src/main/java/ru/example/userservice/controller/MovieController.java
@@ -0,0 +1,29 @@
+package ru.example.userservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/")
+public class MovieController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @RequestMapping("/getAllMovies")
+    public String getAllMovies() {
+        String result = restTemplate.getForObject("http://movie-service/getAll", String.class);
+        return result;
+    }
+
+    @RequestMapping("/get")
+    public String getInfoFromMovieService() {
+        String result = this.restTemplate.getForObject("http://movie-service/", String.class);
+        return result;
+    }
+
+}
